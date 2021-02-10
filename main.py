b915c02ee3b834aa49c31894e1f4bc0e078e122b
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-02-10 14:51:03 -0800

    Add some basic xml parsing

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from src.printFunctions import *
-import json
+import xml.etree.ElementTree as ET
 from os import path
 
-print('this is a test')
+opmlFileName = './Main.opml'
+
+tree = ET.parse('./Main.opml')
+root = tree.getroot()
+for outline in root.iter('outline'):
+  text = outline.attrib['text']
+  print(text)
