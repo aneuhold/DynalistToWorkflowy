89af82eb3eb0f8d1b7972f341b583192ea37b2e3
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-02-11 07:09:25 -0800

    Add beginning of date parsing

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from src.printFunctions import *
 import xml.etree.ElementTree as ET
 from os import path
+import re
+from src.conversion import convertDates
 
 opmlFileName = './Main.opml'
 
 tree = ET.parse('./Main.opml')
 root = tree.getroot()
+convertDates('If they really need to pay early then according to Amr !(2020-12-22) , Amr can setup a manual invoice thr !(2020-12-22)')
+
+'''
 for outline in root.iter('outline'):
   text = outline.attrib['text']
-  print(text)
+  convertDates(text)
+'''
